2148628aca304e9c52d8ccc29b32b09be4d09d4d
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-05-10 03:04:45 +0300

    Item.dart

---
+++ lib/components/item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:toku/models/number.dart';
+
+class Item extends StatelessWidget {
+  const Item({Key? key,required this.number}) : super(key: key);
+   final Number number;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xfff99531),
+      height: 65,
+      child: Row(
+        children: [
+          Container(color: Color(0xfffffce6), child: Image.asset(number.image)),
+          Padding(
+            padding: EdgeInsets.only(left: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  number.jpName,
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                 number.enName,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          Spacer(
+            flex: 1,
+          ),
+          Padding(
+              padding: EdgeInsets.only(right: 16),
+              child: Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+              ))
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/number_page.dart
+++ lib/screens/number_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:toku/components/item.dart';
+import 'package:toku/models/number.dart';
 
 class NumbersPage extends StatelessWidget {
   const NumbersPage({Key? key}) : super(key: key);
@@ -16,50 +18,21 @@ class NumbersPage extends StatelessWidget {
         backgroundColor: Color(0xff49332a),
         title: Text("Number"),
       ),
-      body: Container(
-        color: Color(0xfff99531),
-        height: 65,
-        child: Row(
-          children: [
-            Container(
-                color: Color(0xfffffce6),
-                child: Image.asset(one.image)),
-            Padding(
-              padding: EdgeInsets.only(left: 16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    one.jpName,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Text(
-                    one.enName,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ],
-              ),
-            ),
-            Spacer(
-              flex: 1,
-            ),
-            Padding(
-                padding: EdgeInsets.only(right: 16),
-                child: Icon(
-                  Icons.play_arrow,
-                  color: Colors.white,
-                ))
-          ],
-        ),
-      ),
+      body: Column(
+        children: [
+
+          Item(number: one),
+          Item(number: one),
+          Item(number: one),
+          Item(number: one),
+          Item(number: one),
+          Item(number: one),
+          Item(number: one),
+          Item(number: one),
+          Item(number: one),
+        ],
+      )
     );
   }
 }
 
-class Number {
-  final String image;
-  final String jpName;
-  final String enName;
-  const Number(
-      {required this.image, required this.jpName, required this.enName});
-}
