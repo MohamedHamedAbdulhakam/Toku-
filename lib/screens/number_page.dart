f45e25ff362ca7c29a9d38ddb4d205d3ef93aac1
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-05-12 23:44:27 +0300

    creating list of numbers

--- lib/screens/number_page.dart
+++ lib/screens/number_page.dart
@@ -6,11 +6,71 @@ import 'package:toku/models/number.dart';
 
 class NumbersPage extends StatelessWidget {
   const NumbersPage({Key? key}) : super(key: key);
-  final Number one = const Number(
-      image: "assets/images/numbers/number_one.png",
-      jpName: "ichi",
-      enName: "one");
-
+  
+     final List<Number> numbers = const [
+    Number(
+      
+      jpName: 'ichi',
+      enName: 'one',
+      image: 'assets/images/numbers/number_one.png',
+    ),
+    Number(
+     
+      jpName: 'Ni',
+      enName: 'two',
+      image: 'assets/images/numbers/number_two.png',
+    ),
+    Number(
+   
+      jpName: 'San',
+      enName: 'three',
+      image: 'assets/images/numbers/number_three.png',
+    ),
+    Number(
+     
+      jpName: 'Shi',
+      enName: 'four',
+      image: 'assets/images/numbers/number_four.png',
+    ),
+    Number(
+      
+      jpName: 'Go',
+      enName: 'five',
+      image: 'assets/images/numbers/number_five.png',
+    ),
+    Number(
+     
+      jpName: 'Roku',
+      enName: 'six',
+      image: 'assets/images/numbers/number_six.png',
+    ),
+    Number(
+      
+      jpName: 'Sebun',
+      enName: 'seven',
+      image: 'assets/images/numbers/number_seven.png',
+    ),
+    Number(
+     
+      jpName: 'hachi',
+      enName: 'eight',
+      image: 'assets/images/numbers/number_eight.png',
+    ),
+    Number(
+      
+      jpName: 'Kyū',
+      enName: 'nine',
+      image: 'assets/images/numbers/number_nine.png',
+    ),
+    Number(
+      
+      jpName: 'Jū',
+      enName: 'ten',
+      image: 'assets/images/numbers/number_ten.png',
+    ),
+  ];
+    
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,42 +80,18 @@ class NumbersPage extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
-            Item(number: one),
+            Item(number: numbers[0]),
+            Item(number: numbers[1]),
+            Item(number: numbers[2]),
+            Item(number: numbers[3]),
+            Item(number: numbers[4]),
+            Item(number: numbers[5]),
+            Item(number: numbers[6]),
+            Item(number: numbers[7]),
+            Item(number: numbers[8]),
+            Item(number: numbers[9]),
+         
+           
           ],
         ));
   }
