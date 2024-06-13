8778675d372412e940ea436cb3f09dba692a3035
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-06-13 21:56:27 +0300

    adding pakages

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class category extends StatelessWidget {
   category({this.text, this.color, this.onTap});
